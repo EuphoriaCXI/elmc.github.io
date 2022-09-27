dbd9e565826f1cbb3d1c3e17dbc8e50bb621ea42
Erhan Karakuyu <erhan.k123@gmail.com>
2022-09-27 17:11:58 +1000

    Added more HTML / QOL changes

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const menuBtn = document.querySelector(".hamburger");
 const mobileMenu = document.querySelector(".hamburger-nav");
 const navCta = document.querySelector(".nav-cta");
 
+// Hamburger - It's only function is to toggle the hamburger active or in-active
 menuBtn.addEventListener("click", function () {
   menuBtn.classList.toggle("is-active");
   mobileMenu.classList.toggle("is-active");
